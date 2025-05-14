69dd89bde16c7953e3852bb0a42ef5d3ff3d12c3
yanuo.zhang <z360162883@163.com>
2025-05-14 22:23:06 +1000

    add navigation

--- app/src/main/java/com/example/monashswap/AppNavHost.kt
+++ app/src/main/java/com/example/monashswap/AppNavHost.kt
@@ -4,8 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.monashswap.screens.LoginScreen
+import com.example.monashswap.pages.LoginScreen
 import com.example.monashswap.screens.MonashMPScreen
+import com.example.monashswap.screens.RegisterScreen
 import com.example.monashswap.screens.SplashScreen
 
 @Composable
@@ -16,7 +17,19 @@ fun AppNavHost(navController: NavHostController) {
                 popUpTo("splash") { inclusive = true }
             }
         }) }
-        composable("login") { LoginScreen(navController) }
+        composable("login") { LoginScreen(
+            onRegisterClick = {
+                navController.navigate("register")
+            },
+            onLoginSuccess = {
+                navController.navigate("main") {
+                    popUpTo("login") { inclusive = true }
+                }
+            })
+        }
+        composable("register") {
+            RegisterScreen()
+        }
         composable("main") { MonashMPScreen(navController) }
     }
 }
