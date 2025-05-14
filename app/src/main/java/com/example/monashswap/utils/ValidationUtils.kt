d018c406f2030313db8edd93d4229aeaccf683be
yanuo.zhang <z360162883@163.com>
2025-05-14 22:24:46 +1000

    create utils

---
+++ app/src/main/java/com/example/monashswap/utils/ValidationUtils.kt
@@ -0,0 +1,7 @@
+package com.example.monashswap.utils
+
+fun String.isValidEmail(): Boolean =
+    "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$".toRegex().matches(this)
+
+fun String.isValidPassword(): Boolean =
+    Regex("^(?=.*[0-9])(?=.*[!@#\$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])(.{8,})$").matches(this)
