c023e5262dee4bd35390c0aefe6be92c5b838e00
Mo-Feng-33786267 <mfen0018@student.monash.edu>
2025-05-21 23:25:12 +1000

    Finished product entity class

--- app/src/main/java/com/example/monashMP/data/entity/ProductEntity.kt
+++ app/src/main/java/com/example/monashMP/data/entity/ProductEntity.kt
@@ -1,4 +1,30 @@
+
 package com.example.monashMP.data.entity
 
-class ProductEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+data class ProductEntity(
+    @PrimaryKey(autoGenerate = true) val productId: Long = 0,
+    val sellerUid: String = "",
+    val photos: List<String> = emptyList(),
+    val title: String = "",
+    val desc: String = "",
+    val price: Float = 0f,
+    val category: String = "",
+    val condition: String = "",
+    val location: String = "",
+    val meetupPoint: String = "",
+    val dayPreferenceWeekdays: Boolean = false,
+    val dayPreferenceWeekends: Boolean = false,
+    val paymentMethodPreference: String = "",
+    val additionalNotes: String = "",
+    val email: String = "",
+    val phoneNum: String = "",
+    val viewCount: Int = 0,
+    val preferredContactMethod: String = "",
+    val createdAt: Long = System.currentTimeMillis(),
+    val isActive: Boolean = true,
+    val isSynced: Boolean = false
+)
