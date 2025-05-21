a783538fb2a3ada39bd533747b166c872298c3a0
Mo-Feng-33786267 <mfen0018@student.monash.edu>
2025-05-22 00:13:19 +1000

    Finished database class

--- app/src/main/java/com/example/monashMP/data/AppDatabase.kt
+++ app/src/main/java/com/example/monashMP/data/AppDatabase.kt
@@ -1,4 +1,46 @@
 package com.example.monashMP.data
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.example.monashMP.data.dao.ProductDao
+import com.example.monashMP.data.entity.ProductEntity
+import com.example.monashMP.utils.StringListConverter
+
+@Database(
+    entities = [
+        ProductEntity::class,
+    ],
+    version = 1,
+    exportSchema = false
+)
+@TypeConverters(StringListConverter::class)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun productDao(): ProductDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        /**
+         * Returns a singleton instance of AppDatabase
+         */
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                )
+                    .fallbackToDestructiveMigration(false)
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
